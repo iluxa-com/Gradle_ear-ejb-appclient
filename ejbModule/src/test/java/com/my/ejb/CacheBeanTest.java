e5f11661e5f242503c2cc5a48c0a8aeb3ab2aae8
Marcin Milewski <marcin.milewski@mail.com>
2015-05-12 13:56:34 +0200

    arquillian test ok

---
+++ ejbModule/src/test/java/com/my/ejb/CacheBeanTest.java
@@ -0,0 +1,17 @@
+package com.my.ejb;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by marcin on 12.05.15.
+ */
+public class CacheBeanTest {
+
+        @Test
+        public void should_create_greeting() {
+            //Assert.fail("Not yet implemented");
+            Assert.assertTrue("1 equal 1 - fail",1==1 );
+        }
+
+}
